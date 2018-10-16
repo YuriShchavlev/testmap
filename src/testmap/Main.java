ca1075f1d93733799d1e262c548297da28a519bd
Yuri <yurishchavlev@gmail.com>
2018-10-16 18:57:24 +0300

    Hash-project

---
+++ src/testmap/Main.java
@@ -0,0 +1,25 @@
+package testmap;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map<String, String> translations = new TreeMap<>(Comparator.reverseOrder());
+
+	translations.put("one","один");
+        translations.put("home","дом");
+        translations.put("mouse","мышь");
+
+     //   System.out.println(translations.get("home"));
+
+        for (String k: translations.keySet()){
+
+           System.out.printf("%s -> %s\n", k, translations.get(k));
+        }
+
+    }
+
+    //check Java-version!!!
+
+}
